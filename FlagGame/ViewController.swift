e412e3abbf080e06b60b9f0f17eeea702911dd91
Tuba Nur <yasatubanur@gmail.com>
2022-02-22 14:04:12 +0300

    Initial Commit

---
+++ FlagGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagGame
+//
+//  Created by Tuba Nur YAŞA on 22.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
